cbb628907ec2d2f81d3e713f4173f0f4a06fa2f8
rdmoll <ryan.d.moll@gmail.com>
2021-02-14 20:46:07 -0500

    Corrections to BS code

--- BlackScholes.py
+++ BlackScholes.py
@@ -4,30 +4,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import special
 
-S_low = 150.0
-S_high = 220.0
-dS = 1.0
-K = 190.0
-T = 0.45
-dt = 0.1
-r = 0.001
-sigma = 0.9
-
-#S_low = 70.0
-#S_high = 130.0
+#S_low = 150.0
+#S_high = 220.0
 #dS = 1.0
-#K = 100.0
-#T = 1.0
+#K = 190.0
+#T = 0.45
 #dt = 0.1
-#r = 0.12
-#sigma = 0.1
+#r = 0.001
+#sigma = 0.9
+
+S_low = 70.0
+S_high = 130.0
+dS = 1.0
+K = 100.0
+T = 1.0
+dt = 0.1
+r = 0.12
+sigma = 0.1
 
 t = np.arange(0.0, T, dt)
 S = np.arange(S_low, S_high, dS)
 t, S = np.meshgrid(t, S)
 
-d1 = ( np.log(S/K) + (r + 0.5*(sigma**2))*(T-t) ) / sigma*np.sqrt(T-t)
-d2 = ( np.log(S/K) + (r - 0.5*(sigma**2))*(T-t) ) / sigma*np.sqrt(T-t)
+d1 = ( np.log(S/K) + (r + 0.5*(sigma**2))*(T-t) ) / ( sigma*np.sqrt(T-t) )
+d2 = ( np.log(S/K) + (r - 0.5*(sigma**2))*(T-t) ) / ( sigma*np.sqrt(T-t) )
 
 Phi1_call = 0.5*(special.erf(d1/np.sqrt(2.0)) + 1.0)
 Phi2_call = 0.5*(special.erf(d2/np.sqrt(2.0)) + 1.0)
