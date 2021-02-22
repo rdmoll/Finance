286277bb46f64c5e92fb2f175677de1f2e45b241
rdmoll <ryan.d.moll@gmail.com>
2021-02-21 20:01:50 -0500

    Added geometric Brownian motion function

--- KellyCriterion.py
+++ KellyCriterion.py
@@ -2,42 +2,51 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import financeFunctions as ff
 
 # Single asset
 
 # Define parameters
-T = 1.0
-dt = 0.001
-t = np.arange(0.0, T, dt)
+#T = 1.0
+#dt = 0.001
+#t = np.arange(0.0, T, dt)
 
-S0 = 1.0
-mu = 0.01
-r = 0.0085
-sigma = 0.05
-sigma2 = sigma**2
-f = (mu - r) / sigma2
+#S0 = 1.0
+#mu = 0.01
+#r = 0.0085
+#sigma = 0.05
+#sigma2 = sigma**2
+#f = (mu - r) / sigma2
 
-print(f)
-print()
+#print(f)
+#print()
 
 # Brownian motion
-W = np.zeros(t.size)
-N = np.random.normal(0.0, sigma, t.size-1)
-for i in np.arange(t.size-1):
-    W[i+1] = W[i] + N[i]
-
-S = S0 * np.exp((mu - 0.5*sigma2)*t + sigma*W)
-drift = S0 * np.exp(mu*t)
+#W = np.zeros(t.size)
+#N = np.random.normal(0.0, sigma, t.size-1)
+#for i in np.arange(t.size-1):
+#    W[i+1] = W[i] + N[i]
+#
+#S = S0 * np.exp((mu - 0.5*sigma2)*t + sigma*W)
+#drift = S0 * np.exp(mu*t)
 
 # generate simulated stock history
 # set up time step loop
-for i in np.arange(0,t.size):
+#for i in np.arange(0,t.size):
     #position = f * S[i]
     #cash = prevPos + cash
     # calculate leveraged position and equity at each time step based on f
 
+num_ts = 100
+mu = 0.01
+sigma = 0.05
+
+t = np.arange(num_ts)
+S = ff.gbmSimulation(num_ts, mu, sigma)
+
 # Plot results
 plt.figure()
-plt.plot(t,S)
-plt.plot(t,drift)
+plt.plot(t,S[:,0])
+#plt.plot(t,S)
+#plt.plot(t,drift)
 plt.show()
---
+++ financeFunctions.py
@@ -0,0 +1,21 @@
+# Geometric Brownian Motion
+
+import numpy as np
+
+def gbmSimulation(num_ts, mu, sigma, S0 = 1.0):
+    sigma2 = sigma * sigma
+    drift = mu - 0.5*sigma2
+    S = np.zeros((num_ts,3))
+
+    N = np.random.normal(0.0, 1.0, num_ts-1)
+
+    S[0,0] = S0
+    S[0,1] = 0.0
+    S[0,2] = 0.0
+    for i in np.arange(1, num_ts):
+        S[i,2] = S[i-1,2] + N[i-1]
+        pctChange = np.exp( (drift*i) + (sigma*S[i,2]) )
+        S[i,0] = S0 * pctChange
+        S[i,1] = pctChange
+
+    return S
