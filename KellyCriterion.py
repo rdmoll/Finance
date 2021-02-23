0b38252d50c1da57385df76272a340c8ba575771
rdmoll <ryan.d.moll@gmail.com>
2021-02-22 21:54:41 -0500

    Fixing single asset Kelly criterion code

--- KellyCriterion.py
+++ KellyCriterion.py
@@ -6,47 +6,35 @@ import financeFunctions as ff
 
 # Single asset
 
-# Define parameters
-#T = 1.0
-#dt = 0.001
-#t = np.arange(0.0, T, dt)
-
-#S0 = 1.0
-#mu = 0.01
-#r = 0.0085
-#sigma = 0.05
-#sigma2 = sigma**2
-#f = (mu - r) / sigma2
-
-#print(f)
-#print()
-
-# Brownian motion
-#W = np.zeros(t.size)
-#N = np.random.normal(0.0, sigma, t.size-1)
-#for i in np.arange(t.size-1):
-#    W[i+1] = W[i] + N[i]
-#
-#S = S0 * np.exp((mu - 0.5*sigma2)*t + sigma*W)
-#drift = S0 * np.exp(mu*t)
-
-# generate simulated stock history
-# set up time step loop
-#for i in np.arange(0,t.size):
-    #position = f * S[i]
-    #cash = prevPos + cash
-    # calculate leveraged position and equity at each time step based on f
-
 num_ts = 100
 mu = 0.01
 sigma = 0.05
+r = 0.0085
+
+f = (mu - r) / (sigma * sigma)
+
+V_c = 100.0
+V_i = f * V_c
 
 t = np.arange(num_ts)
 S = ff.gbmSimulation(num_ts, mu, sigma)
 
+V = np.zeros((num_ts,3))
+V[0,0] = V_i
+V[0,1] = V_c
+V[0,2] = V_i + V_c
+for i in np.arange(1,num_ts):
+  V_i = V_i * S[i,1]
+  V_tot = V_i + V_c
+  V_i = f * V_tot
+  V_c = (1 - f) * V_tot
+  V[i,0] = V_i
+  V[i,1] = V_c
+  V[i,2] = V_i + V_c
+
 # Plot results
 plt.figure()
 plt.plot(t,S[:,0])
-#plt.plot(t,S)
-#plt.plot(t,drift)
+plt.figure()
+plt.plot(t,V[:,2])
 plt.show()
